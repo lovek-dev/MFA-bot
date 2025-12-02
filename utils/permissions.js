d3e6e0ebcdd291e1ba778f9e073bd75602fc6ffb
lovek-dev <lovekumar0412@gmail.com>
2025-12-02 20:24:45 +0530

    Update permissions.js

--- utils/permissions.js
+++ utils/permissions.js
@@ -1,15 +1,14 @@
 import config from "../config.json" assert { type: "json" };
 import fs from "fs";
 
-// Check if user has allowed role OR is owner
 export function hasBotAccess(member) {
-    if (member.id === config.ownerId) return true; // owner bypass
-    return member.roles.cache.has(config.allowedRoleId);
+  if (member.id === config.ownerId) return true;
+  return member.roles.cache.has(config.allowedRoleId);
 }
 
-// Update allowed role inside config.json
 export function updateAllowedRole(roleId) {
-    config.allowedRoleId = roleId;
-    fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+  config.allowedRoleId = roleId;
+  fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
 }
+
 
