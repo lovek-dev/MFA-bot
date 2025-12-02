92e48318ac4c739c8419d0342f4d160736c3f0e1
lovek-dev <lovekumar0412@gmail.com>
2025-12-02 20:24:00 +0530

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,7 @@
 import config from "../config.json" assert { type: "json" };
 
 export async function logAction(guild, embed) {
-    const channel = guild.channels.cache.get(config.logChannelId);
-    if (!channel) return;
-    channel.send({ embeds: [embed] });
+  const ch = guild.channels.cache.get(config.logChannelId);
+  if (!ch) return;
+  await ch.send({ embeds: [embed] }).catch(() => {});
 }
