aa0d2a2975520e8f0dcd73f673974b31b8af591b
lovek-dev <lovekumar0412@gmail.com>
2025-12-02 19:26:41 +0530

    Update moderation.js

--- commands/prefix/moderation.js
+++ commands/prefix/moderation.js
@@ -1,11 +1,17 @@
 import { EmbedBuilder, PermissionsBitField } from "discord.js";
+import { hasBotAccess } from "../../utils/permissions.js";
 import { logAction } from "../../utils/logger.js";
 
 export default {
     name: "kick",
     run: async (client, message, args) => {
+
+        // Bot access check
+        if (!hasBotAccess(message.member))
+            return message.reply("❌ You are not allowed to use this bot.");
+
         if (!message.member.permissions.has(PermissionsBitField.Flags.KickMembers))
-            return message.reply("❌ You don't have permission.");
+            return message.reply("❌ You don't have permission to kick.");
 
         const user = message.mentions.members.first();
         if (!user) return message.reply("Mention someone to kick.");
@@ -18,7 +24,6 @@ export default {
             .setColor("Red");
 
         logAction(message.guild, embed);
-
         message.reply("User kicked.");
     }
 };
