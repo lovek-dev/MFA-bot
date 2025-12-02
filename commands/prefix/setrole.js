5a21b3d359c0b4a1a8a36e61eb40b004f0a01abd
lovek-dev <lovekumar0412@gmail.com>
2025-12-02 20:28:02 +0530

    Update setrole.js

--- commands/prefix/setrole.js
+++ commands/prefix/setrole.js
@@ -1,15 +1,17 @@
-const fs = require("fs");
-const config = require("../../config.json");
+import { updateAllowedRole } from "../../utils/permissions.js";
+import { hasBotAccess } from "../../utils/permissions.js";
 
-module.exports = {
+export default {
   name: "setrole",
   async run(client, message, args) {
+    if (!hasBotAccess(message.member))
+      return message.reply("❌ You cannot use this bot.");
+
     const role = message.mentions.roles.first();
     if (!role) return message.reply("Mention a role.");
 
-    config.allowedRole = role.id;
-    fs.writeFileSync("./config.json", JSON.stringify(config, null, 2));
-
+    updateAllowedRole(role.id);
     message.reply(`✅ Allowed role set to: ${role.name}`);
   }
 };
+
