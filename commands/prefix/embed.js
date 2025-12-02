8defbeeedb9bbdde9681ea09dcc5c4716d25aa47
lovek-dev <lovekumar0412@gmail.com>
2025-12-02 20:06:27 +0530

    Create embed.js

---
+++ commands/prefix/embed.js
@@ -0,0 +1,13 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "embed",
+  run(client, message, args) {
+    const embed = new EmbedBuilder()
+      .setTitle(args[0] || "No Title")
+      .setDescription(args.slice(1).join(" ") || "No Description")
+      .setColor("Random");
+
+    message.channel.send({ embeds: [embed] });
+  }
+};
