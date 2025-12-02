b58c53e6b12c0936ee8cf3afa1df5ef908dc73fc
lovek-dev <lovekumar0412@gmail.com>
2025-12-02 19:27:48 +0530

    Update dmrole.js

--- commands/prefix/dmrole.js
+++ commands/prefix/dmrole.js
@@ -1,16 +1,22 @@
+import { hasBotAccess } from "../../utils/permissions.js";
+
 export default {
     name: "dmrole",
     run: async (client, message, args) => {
+
+        if (!hasBotAccess(message.member))
+            return message.reply("❌ You are not allowed to use this bot.");
+
         const role = message.mentions.roles.first();
         if (!role) return message.reply("Mention a role.");
 
-        let sent = 0;
-
         message.reply(`Sending DMs to ${role.members.size} users...`);
+
+        let sent = 0;
 
         for (const member of role.members.values()) {
             try {
-                await member.send(`Message from ${message.author}: ${args.slice(1).join(" ")}`);
+                await member.send(args.slice(1).join(" "));
                 sent++;
                 await new Promise(r => setTimeout(r, 1000));
             } catch {}
